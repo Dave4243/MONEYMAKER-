b1dddfb5f2d1d1e248d7c01cf6659c3d78eda853
David Huang <davidhuang.oc@gmail.com>
2024-11-02 21:50:50 -0500

    Abstracted contents into function get_stock_data_normalized

--- stock_data.py
+++ stock_data.py
@@ -2,19 +2,23 @@ import yfinance as yf
 import datetime
 import numpy as np
 
-# Set the ticker symbol and date range
-ticker = "DJT"
-end_date = datetime.date.today()
-start_date = end_date - datetime.timedelta(days=30)
+# ticker is a string
+# num days is a int
+# max_value is a double
+def get_stock_data_normalized(ticker, num_days, max_value):
+  end_date = datetime.date.today()
+  start_date = end_date - datetime.timedelta(days = num_days)
 
-# Fetch the data
-stock_data = yf.download(ticker, start=start_date, end=end_date)
+  # Fetch the data
+  stock_data = yf.download(ticker, start=start_date, end=end_date)
 
-# convert DataFrame to list
-closing_prices_list = np.ravel(stock_data["Close"].values.tolist())
-print(closing_prices_list)
+  # convert DataFrame to list
+  closing_prices_list = np.ravel(stock_data["Close"].values.tolist())
+  print(closing_prices_list)
 
-closing_prices_list *= (1.0 / closing_prices_list.max())
+  closing_prices_list *= (max_value / closing_prices_list.max())
 
-print('\n')
-print(closing_prices_list)
+  print('\n')
+  print(closing_prices_list)
+
+get_stock_data_normalized("DJT", 30, 1.0)
